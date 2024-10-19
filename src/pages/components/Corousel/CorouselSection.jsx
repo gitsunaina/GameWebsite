4d592c6f5a062e6c52dbc2990ce1d8dccaca0a08
sunaina26 <138777512+gitsunaina@users.noreply.github.com>
2024-10-20 05:21:12 +0530

    make responsive

--- src/pages/components/Corousel/CorouselSection.jsx
+++ src/pages/components/Corousel/CorouselSection.jsx
@@ -1,54 +1,50 @@
-import React, { useMemo, useRef } from 'react'
-import Cards from './Cards';
-import Slider from 'react-slick';
-
+import React, { useMemo, useRef } from "react";
+import Cards from "./Cards";
+import Slider from "react-slick";
 
 const CorouselSection = () => {
-    const settings = useMemo(() => ({
-        dots: true,
-        infinite: true,
-        slidesToShow: 2.7, // Show 2 full cards and half of the third
-        slidesToScroll: 1,
-        arrows: false,
-        // centerMode: false, // Ensure center mode is disabled
-        responsive: [
-          {
-            breakpoint: 1024, // For medium to smaller screens
-            settings: {
-              slidesToShow: 2, // Show 1 full card and half of the next on smaller screens
-              slidesToScroll: 1,
-            },
+  const settings = useMemo(
+    () => ({
+      dots: true,
+      infinite: true,
+      slidesToShow: 2.7,
+      slidesToScroll: 1,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
           },
-          {
-            breakpoint: 768, // For mobile screens
-            settings: {
-//               infinite: true,
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              // arrows: false,
-              dots: true,
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
           },
-          },
-        ],
-    }), []);
+        },
+      ],
+    }),
+    []
+  );
 
-    const slider = useRef(null);
+  const slider = useRef(null);
   return (
     <div className="relative h-full">
-        <div className="lg:w-[100%] h-full lg:mx-auto">
-          <Slider
-            {...settings}
-            ref={slider}
-          >
-<Cards />
-<Cards />
-<Cards />
-<Cards />
-<Cards />
-          </Slider>
-          </div>
-          </div>
-  )
-}
+      <div className="lg:w-[100%] h-full lg:mx-auto">
+        <Slider {...settings} ref={slider}>
+          <Cards />
+          <Cards />
+          <Cards />
+          <Cards />
+          <Cards />
+        </Slider>
+      </div>
+    </div>
+  );
+};
 
-export default CorouselSection
+export default CorouselSection;
